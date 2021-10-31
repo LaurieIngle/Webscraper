5ae15ca939de5f8b85b43e240ff4b22d09c5132f
LaurieInlge <laurieingle@hotmail.com>
2021-10-31 17:33:45 +1100

    Park project

---
+++ Webscraper.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+Websites = pd.read_csv (r'/Documents/Coding projects/Caravan Park Websites.csv')
+print(websites)
+html_text = requests.get('https://www.campsitephotos.com/rv-campground/ca/angels-camp-rv-camping-resort/').text
+soup = BeautifulSoup(html_text)
+address = soup.find('address').text
+print(address)
